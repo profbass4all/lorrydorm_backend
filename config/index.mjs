7829dfd94379adac1335c7e603d506fe3ea823eb
Abass Ibrahim <profbass4all@gmail.com>
2025-01-05 14:30:13 +0100

    editted config folder

--- config/index.mjs
+++ config/index.mjs
@@ -5,7 +5,7 @@ import { Sequelize } from 'sequelize';
 export const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
     host: process.DB_HOST,
     dialect: 'mysql',
-    port: process.env.DB_PORT
+    port: process.env.DB_PORT || 3306
 });
 
 //this function establish connection
@@ -19,4 +19,3 @@ export default async function connection (){
         }
 
 }
-
